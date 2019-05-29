a6a6d1e2c39f34e1143fa7a23c37a0c50b6d1cd7
Praveen Raja <51159976+Praveen-Ellora@users.noreply.github.com>
2019-05-29 17:03:56 +0530

    Update Testclass.java

--- PraseeGit/src/Testclass.java
+++ PraseeGit/src/Testclass.java
@@ -7,6 +7,7 @@ public static void main(String args[])
 	System.out.println("Git repository is added successfully");
 	System.out.println("Git repository is added successfully");
 	System.out.println("new branch added");
+	System.out.println("new branch added");
 	
 	int a=1;
 	int b=2;
