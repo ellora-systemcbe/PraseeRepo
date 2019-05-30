2bf2a9cfba01445720b3b104fefb2742b6ca10ad
praveen <praveen@TESTING-PRAVEEN>
2019-05-30 09:45:41 +0530

    Praveen Chnages

--- PraseeGit/src/Testclass.java
+++ PraseeGit/src/Testclass.java
@@ -7,7 +7,7 @@ public static void main(String args[])
 	System.out.println("Git repository is added successfully");
 	System.out.println("Git repository is added successfully");
 	System.out.println("new branch added");
-	System.out.println("Raajathi Raajan----Vera Level Bro...");
+	System.out.println("Raajathi Raajan----Vera Level Bro89897889...");
 	
 	int a=1;
 	int b=2;
